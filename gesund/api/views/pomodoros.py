f4e5e34f02ba974b89c32e20bd9d851cb0bcadb9
asm <dev@asm.com.np>
2022-07-16 16:56:54 +0200

    update

--- gesund/api/views/pomodoros.py
+++ gesund/api/views/pomodoros.py
@@ -1,10 +1,12 @@
 from api.serializers import PomodoroSerializer
 from pomodoros.models import Pomodoro
+from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveAPIView
 
 
 class PomodoroListCreateAPIView(ListCreateAPIView):
     """ Lists or Create pomodoro(s). """
+    permission_classes = (permissions.IsAuthenticated)
     queryset = Pomodoro.objects.all()
     serializer_class = PomodoroSerializer
 
