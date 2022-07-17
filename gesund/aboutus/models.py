90281be5a75d2f5d4615cde1860f146dbf989d52
asm <dev@asm.com.np>
2022-07-17 10:40:58 +0200

    added contactus model

---
+++ gesund/aboutus/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+
+
+class ContactUs(models.Model):
+    """ Contact Us model. """
+    id = models.AutoField(primary_key=True, editable=False)
+    datestamp = models.DateField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('about')
