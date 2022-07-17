cad0a50ae9edfcb538fe317b039f02d9358e8d79
asm <dev@asm.com.np>
2022-07-17 19:48:16 +0200

    update

--- gesund/pomodoros/views/list_by_date.py
+++ gesund/pomodoros/views/list_by_date.py
@@ -3,10 +3,16 @@ from django.views.generic import ListView
 from pomodoros.models import Pomodoro
 
 
+def get_total_pomodoro(usr, datestamp):
+    return Pomodoro.objects.raw('''SELECT sum(pomodoro) FROM pomodoros_pomodoro 
+        WHERE author_id=%s AND datestamp=%s''', [usr, datestamp])
+
+
 class PomodoroListViewByDate(LoginRequiredMixin, ListView):
     """ List all pomodoros by datestamp. """
     context_object_name = 'pomodoro_list'
     model = Pomodoro
+    paginate_by = 10
     template_name = 'pomodoros/list_by_date.html'
 
     def get_queryset(self):
@@ -17,5 +23,12 @@ class PomodoroListViewByDate(LoginRequiredMixin, ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(PomodoroListViewByDate, self).get_context_data(*args, **kwargs)
+
         context['datestamp'] = self.kwargs['datestamp']
+
+        context['total_pomodoro'] = get_total_pomodoro(
+            self.request.user.id,
+            context['datestamp']
+        )
+
         return context
